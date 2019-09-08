d6deaa87ec2324ed57782d97cff9139b4431aeee
Robert Kühne <sponiro@gmail.com>
2019-09-08 11:25:31 +0200

    Add solution

--- src/main/java/de/fanero/tdd/WordStatistics.java
+++ src/main/java/de/fanero/tdd/WordStatistics.java
@@ -1,12 +1,26 @@
 package de.fanero.tdd;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class WordStatistics {
 
     public static Map<Integer, Integer> createStatistics(List<String> words) {
 
-        return null;
+        Map<Integer, Integer> stats = new HashMap<>();
+
+        words.stream()
+                .filter(Objects::nonNull)
+                .forEach(word -> {
+                    stats.compute(word.length(), (key, value) -> {
+
+                        int oldValue = value == null ? 0 : value;
+                        return oldValue + 1;
+                    });
+                });
+
+        return stats;
     }
 }
--- src/test/java/de/fanero/tdd/WordStatisticsTest.java
+++ src/test/java/de/fanero/tdd/WordStatisticsTest.java
@@ -2,12 +2,44 @@ package de.fanero.tdd;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class WordStatisticsTest {
 
     @Test
-    public void dummy() {
-        fail();
+    public void emptyWords() {
+
+        Map<Integer, Integer> statistics = WordStatistics.createStatistics(Collections.<String>emptyList());
+
+        assertThat(statistics.size(), is(0));
+    }
+
+    @Test
+    public void singleWord() {
+
+        Map<Integer, Integer> statistics = WordStatistics.createStatistics(Arrays.asList("word"));
+
+        assertThat(statistics.get(4), is(1));
+    }
+
+    @Test
+    public void twoWords() {
+
+        Map<Integer, Integer> statistics = WordStatistics.createStatistics(Arrays.asList("word", "moly"));
+
+        assertThat(statistics.get(4), is(2));
+    }
+
+    @Test
+    public void nullWord() {
+
+        Map<Integer, Integer> statistics = WordStatistics.createStatistics(Arrays.asList((String) null));
+
+        assertThat(statistics.size(), is(0));
     }
 }
